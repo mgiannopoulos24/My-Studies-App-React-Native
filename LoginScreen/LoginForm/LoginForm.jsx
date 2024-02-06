7cfa57b7d11f19cb4613a170bc281299d6860395
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2024-02-06 20:30:13 +0200

    Initial Commit

---
+++ LoginScreen/LoginForm/LoginForm.jsx
@@ -0,0 +1,40 @@
+import React, {useState} from "react";
+import { View, StyleSheet,TextInput} from 'react-native';
+
+const LoginForm=()=>{
+    const [text, setText] = useState('');
+    const onChangeText = newText => {
+        setText(newText);
+    };
+    return(
+        <View>
+            <TextInput
+                style={styles.input}
+                onChangeText={onChangeText}
+                value={text}
+                placeholder="Email"
+            />
+            <TextInput
+                style={styles.input}
+                onChangeText={onChangeText}
+                value={text}
+                placeholder="Κωδικός"
+            />
+
+        </View>
+    )
+}
+
+const styles= StyleSheet.create({
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        backgroundColor:"white",
+        borderRadius:10
+      },
+})
+
+
+export default LoginForm;
