3e9cd49dbeb1bf06a46156acc286becbb7091268
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2024-02-09 17:17:56 +0200

    Initial Commit

---
+++ MainPage/MainStud.jsx
@@ -0,0 +1,13 @@
+import { View,Text } from "react-native";
+import DrawerStud from "../Drawers/DrawerStud";
+
+const MainStud=()=>{
+    return(
+        <View>
+            <Text>Kalimera</Text>
+            <DrawerStud/>
+        </View>
+    )
+}
+
+export default MainStud;
