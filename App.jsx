8ed1fcb3428860462c3befa8e6528a43aabc901c
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2024-02-09 17:17:46 +0200

    Added MainStud

--- App.jsx
+++ App.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+import { StyleSheet, View} from 'react-native';
 import Login from "./LoginScreen/Login";
-import InfoIcon from './InfoIcon/InfoIcon';
+import MainStud from './MainPage/MainStud';
+
 
 
 
 export default function App (){
   return (
     <View style={{ position : 'absolute', top : 0, left : 0, right : 0,bottom : 0,}}>
-        <Login/>
+        {/* <Login/> */}
+        <MainStud/>
     </View>
   );
 };
