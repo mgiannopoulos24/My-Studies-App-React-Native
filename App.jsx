d4123f03e429863527586b85af96872d62ce9540
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2024-02-10 22:49:53 +0200

    Added DrawerProvider and Nav Container

--- App.jsx
+++ App.jsx
@@ -2,23 +2,22 @@ import React from 'react';
 import { StyleSheet, View} from 'react-native';
 import Login from "./LoginScreen/Login";
 import MainStud from './MainPage/MainStud';
-
-
-
+import { DrawerProvider } from './Drawers/DrawerContext';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App (){
   return (
+    <NavigationContainer>
+    <DrawerProvider>
     <View style={{ position : 'absolute', top : 0, left : 0, right : 0,bottom : 0,}}>
         {/* <Login/> */}
         <MainStud/>
     </View>
+    </DrawerProvider>
+    </NavigationContainer>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex:100,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
+
 });
