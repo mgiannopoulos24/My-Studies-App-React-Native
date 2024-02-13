a72013121518be4deedbc39cbfc28d4f2a36ea15
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2024-02-13 15:39:48 +0200

    Will fix fetchdata soon

--- App.jsx
+++ App.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { StyleSheet, View} from 'react-native';
-import Login from "./LoginScreen/Login";
+import Login from "./src/LoginScreen/Login";
 import MainStud from './MainPage/MainStud';
 import { DrawerProvider } from './Drawers/DrawerContext';
 import { NavigationContainer } from '@react-navigation/native';
-
+import FetchData from './src';
 
 export default function App (){
   return (
     <NavigationContainer>
     <DrawerProvider>
     <View style={{ position : 'absolute', top : 0, left : 0, right : 0,bottom : 0,}}>
-        <Login/>
+        {/* <Login/> */}
+        <FetchData/>
         {/* <MainStud/> */}
     </View>
     </DrawerProvider>
