0c97cba691d0ec56396a91d97d076fe896549963
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2024-02-16 18:36:26 +0200

    Initial Commit

--- src/MainPage/MainProf.jsx
+++ src/MainPage/MainProf.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import Icon from "react-native-vector-icons/FontAwesome6";
+import { useNavigation } from '@react-navigation/native';
+import {useDrawer} from '../Drawers/DrawerContext';
+import DrawerProf from '../Drawers/DrawerProf';
 
 const MainProf = () => {
+  const navigation = useNavigation();
+  const { toggleDrawer } = useDrawer();
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hi</Text>
+        <View style={styles.header}>
+            <TouchableOpacity onPress={toggleDrawer} style={styles.iconContainer}>
+            <Icon name="bars" size={30} color="black" style={{paddingLeft:"30px"}} />
+            </TouchableOpacity>
+            <View style={styles.titleContainer}>
+            <Text style={styles.titleText}>Αρχική Σελίδα</Text>
+            </View>
+        </View>
+        
+        <View style={styles.content}>
+            {/* Your main content goes here */}
+            <Text>Main Content Goes Here</Text>
+        </View>
+        <DrawerProf /> 
     </View>
   );
 };
@@ -12,12 +32,31 @@ const MainProf = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    position: 'relative', // Ensure the position context for zIndex
+  },
+  header: {
+    flexDirection: 'row',
+    height: 50,
+    backgroundColor: 'lightgray',
+  },
+  iconContainer: {
+    width: '5%',
     justifyContent: 'center',
     alignItems: 'center',
   },
-  text: {
-    fontSize: 24,
+  titleContainer: {
+    width: '95%',
+    justifyContent: 'center',
+    paddingLeft: 40,
+  },
+  titleText: {
+    fontSize: 18,
     fontWeight: 'bold',
+  },
+  content: {
+    flex: 1,
+    padding: 10,
+    zIndex: 1, // Ensure content stays below drawer
   },
 });
 
