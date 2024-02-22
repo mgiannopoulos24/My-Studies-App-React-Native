9f2dc87e9704bd88d14ea1f6d0d3f0ebe2e8508e
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2024-02-22 20:08:43 +0200

    Added Message for invalid emails

--- src/LoginScreen/SignupForm/SignupForm.jsx
+++ src/LoginScreen/SignupForm/SignupForm.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import { View, StyleSheet,TextInput,Button} from 'react-native';
+import { View, StyleSheet,TextInput, TouchableOpacity , Text } from 'react-native';
 import { useAuthContext } from '../../Firebase/AuthProvider';
 
 const SignupForm=()=>{
@@ -7,6 +7,17 @@ const SignupForm=()=>{
     const { register } = useAuthContext();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [isValidEmail, setIsValidEmail] = useState(true);
+
+    const validateEmail = (email) => {
+        const regex = /^[^\s@]+@di\.uoa\.gr$/; // Regex to match the required email format
+        return regex.test(email);
+    };
+
+    const handleEmailChange = (email) => {
+        setEmail(email);
+        setIsValidEmail(validateEmail(email));
+    };
 
     const handleRegister = () => {
         register(email, password);
@@ -15,11 +26,12 @@ const SignupForm=()=>{
     return(
         <View style={styles.container}>
         <TextInput
-            style={styles.input}
-            onChangeText={setEmail}
+            style={[styles.input, !isValidEmail && styles.errorInput]}
+            onChangeText={handleEmailChange}
             value={email}
             placeholder="Email"
         />
+        {!isValidEmail && <Text style={styles.errorText}>Το email σας πρέπει να έχει την μορφή sdiXXXXX@di.uoa.gr</Text>}
         <TextInput
             style={styles.input}
             onChangeText={setPassword}
@@ -27,7 +39,11 @@ const SignupForm=()=>{
             placeholder="Κωδικός"
             secureTextEntry={true}
         />
-        <Button title="Εγγραφή" onPress={handleRegister} style={{ width: "50%" }} />
+        <View style={styles.buttonContainer}>
+            <TouchableOpacity onPress={handleRegister} style={styles.button}>
+                <Text style={styles.buttonText}>Εγγραφή</Text>
+            </TouchableOpacity>
+        </View>
     </View>
 
     )
@@ -44,6 +60,27 @@ const styles=StyleSheet.create({
         padding: 10,
         backgroundColor:"white",
         borderRadius:10,
-      },
+    },
+    errorInput: {
+        borderColor: 'red', // Change border color for error state
+    },
+    errorText: {
+        marginLeft: 12,
+        color: 'red', // Text color for error message
+    },
+    buttonContainer: {
+        alignItems: 'center', // Center the button horizontally
+        marginTop: 10,
+    },
+    button: {
+        backgroundColor: 'blue',
+        padding: 10,
+        borderRadius: 5,
+        width: '30%', // Set the button width to 50%
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center', // Center the text inside the button
+    },
 })
 export default SignupForm;
